e9c5dc2930efe98a8c10c75338bea4f535ff0e60
Dominik Pfennig <dominik@suma-ev.de>
2016-05-03 15:30:36 +0200

    Homestead Server sollte nun aufgesetzt werden, falls er nicht vorhanden ist

--- deploy/checkServerStatus.sh
+++ deploy/checkServerStatus.sh
@@ -20,6 +20,27 @@ then
 	cd ~/MetaGer
 	php artisan key:generate
 fi
-    # Falls notwendig Homestead Server intialisieren:
-  #  - cd ~/
-   # - vagrant box add --provider virtualbox laravel/homestead
+# Falls notwendig Homestead Server intialisieren:
+if [ ! -d ~/Homestead ]
+then
+	cd ~/
+	# Wir brauchen unseren öffentlichen ssh Schlüssel:
+	if [ ! -d .ssh ]
+	then
+		mkdir .ssh
+		chmod 700 .ssh
+	fi
+	if [ ! -f .ssh/id_rsa.pub ]
+	then
+		cd .ssh
+		ssh-keygen -t rsa -N "" -f id_rsa
+		cd ~/
+	fi
+	vagrant box add laravel/homestead
+	git clone https://github.com/laravel/homestead.git Homestead
+	cd Homestead/
+	bash init.sh
+	mv -f "$path/deploy/Homestead.yaml" .homestead/
+	vagrant up
+	echo "Your server is now running under http://localhost:8000"
+fi
