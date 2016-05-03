76825757628c33ef1b22120ef76cc5d8d73f7b8f
Dominik Pfennig <dominik@suma-ev.de>
2016-05-03 14:09:24 +0200

    Die Überprüfung, ob der Webserver läuft wurde in ein .sh Skript ausgelagert

---
+++ deploy/checkServerStatus.sh
@@ -0,0 +1,24 @@
+# Klone das Repo neu, falls es noch nicht existiert
+if [ ! -d ~/MetaGer ]
+then 
+	cd ~/
+	git clone "$path" 
+	cd ~/MetaGer
+	composer update 
+	chmod -R 777 storage/ bootstrap/cache
+fi
+# Falls notwendig Laravel-Framework initialisieren:
+
+if [ ! -f ~/MetaGer/.env ]
+then 
+	cd ~/MetaGer
+	cp .env.example .env
+fi
+if [ $(grep "SomeRandomString" .env) ]
+then 
+	cd ~/MetaGer
+	php artisan key:generate
+fi
+    # Falls notwendig Homestead Server intialisieren:
+  #  - cd ~/
+   # - vagrant box add --provider virtualbox laravel/homestead
