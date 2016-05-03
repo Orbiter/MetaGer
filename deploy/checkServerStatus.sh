31b688aa4f85743a384c780314366cace91cb2e2
Dominik Pfennig <dominik@suma-ev.de>
2016-05-03 15:40:26 +0200

    Bugfix

--- deploy/checkServerStatus.sh
+++ deploy/checkServerStatus.sh
@@ -40,7 +40,7 @@ then
 	git clone https://github.com/laravel/homestead.git Homestead
 	cd Homestead/
 	bash init.sh
-	mv -f "$path/deploy/Homestead.yaml" .homestead/
+	cp -f "$path/deploy/Homestead.yaml" .homestead/
 	vagrant up
 	echo "Your server is now running under http://localhost:8000"
 fi
