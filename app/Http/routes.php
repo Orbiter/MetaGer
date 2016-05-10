7382cdffe35d2248350759320952ca66d99c199e
Dominik Pfennig <dominik@suma-ev.de>
2016-05-10 11:30:22 +0200

    Dummy für die Klasse Results eingefügt

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -55,6 +55,8 @@
 
         Route::post('kontakt', 'MailController@contactMail');
 
+        Route::get('meta/meta.ger3', 'MetaGerSearch@search');
+
         Route::get('spende', function()
         {
             return view('spende')
