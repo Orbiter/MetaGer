00dbee23b779220c66e31f08011d87a7bd8c1824
Dominik Pfennig <dominik@suma-ev.de>
2016-05-11 14:01:22 +0200

    Vergessener Merge Commit

--- resources/lang/en/fokiNames.php
+++ resources/lang/en/fokiNames.php
@@ -1,19 +1,10 @@
 <?php
 
 return [
-<<<<<<< HEAD
 	'web' 		=>	"web",
 	'news'		=>	"nachrichten",
 	'science'	=>	'wissenschaft',
 	'shopping'	=>	'produktsuche',
 	'picture'	=>	'bilder',
 	'custom'	=>	'angepasst'
-=======
-	'web' 			=>	"web",
-	'nachrichten'	=>	"news",
-	'wissenschaft'	=>	'science',
-	'produktsuche'	=>	'shopping',
-	'bilder'		=>	'picture',
-	'angepasst'		=>	'custom'
->>>>>>> eeeca90b0d782d61bf3a095b6816645f862cd896
 ];
