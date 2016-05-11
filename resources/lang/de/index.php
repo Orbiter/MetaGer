881c63786557812753f0d8757b5fff7e3032c5d7
Dominik Pfennig <dominik@suma-ev.de>
2016-05-11 13:02:43 +0200

    Eben diese beiden Dateien vergessen

---
+++ resources/lang/de/index.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+	'foki.bilder'	=>	'Bilder',
+	'foki.nachrichten'	=>	'Nachrichten',
+	'foki.wissenschaft'	=>	'Wissenschaft',
+	'foki.produkte'		=>	'Produkte',
+	'foki.anpassen'		=>	'anpassen',
+	'conveyor'			=>	'Einkaufen bei MetaGer-Fördershops',
+	'plugin'			=>	'MetaGer-Plugin hinzufügen',
+	'sponsors'			=>	'Sponsoren',
+	'sponsors.woxikon'	=>	'Woxikon: Nach Wörtern & Synonymen suchen',
+	'sponsors.gutscheine'	=>	'Aktuelle Gutscheine auf Gutschein-Magazin.de',
+	'sponsors.kredite'		=>	'Günstige Kredite auf FINANZCHECK.de vergleichen ',
+	'about.title'			=>	'Über uns',
+	'about.1.1'				=>	'Datenschutz & Privatsphäre',
+	'about.1.2'				=>	'  sind bei uns einfach & selbstverständlich.',
+	'about.2.1'				=>	'Wir arbeiten nicht gewinnorientiert, wir sind ein ',
+	'about.2.2'				=>	'gemeinnütziger Verein. '
+
+];
