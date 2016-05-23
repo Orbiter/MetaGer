1c32b76658378b41407cfe2fa61b061d101a7d3a
karl <karl@suma-ev.de>
2016-05-23 09:26:23 +0200

    + Teamseite übersetzt

---
+++ resources/lang/de/team.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+	'role.1' => 'Geschäftsführer, Vorstand',
+	'role.2' => 'Büroleiter und Jugendschutzbeauftragter',
+	'role.3' => 'Pressereferentin',
+	'role.4' => 'Programmierer',
+	'role.5' => 'Programmierer',
+	'role.6' => 'Programmierer',
+	'role.7' => 'Sekretariat',
+
+	'contact.1' => 'Fragen/Probleme zu MetaGer etc. bitte immer an',
+	'contact.2' => 'mailen, oder das',
+	'contact.3' => 'Kontaktformular mit Verschlüsselung',
+	'contact.4' => 'benutzten, oder am besten: ins',
+	'contact.5' => 'MetaGer-Forum',
+	'contact.6' => 'schreiben. Wir können bei der Vielzahl der Anfragen nicht immer zeitnah antworten - im',
+	'contact.7' => 'MetaGer-Forum',
+	'contact.8' => 'können Ihnen wahrscheinlich auch andere MetaGer-Nutzer weiterhelfen, und von den Fragen und Antworten profitieren ALLE.',
+	'contact.9' => 'Nur in begründeten Ausnahmefällen, wenn Sie bestimmte Personen direkt erreichen wollen, sollten Sie an diese mailen. Denn Team-Mitglieder können Urlaub haben, krank sein usw.',
+];
