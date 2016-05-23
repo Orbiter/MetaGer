03c25b65fa85b4fbf55427a7afb85bd3aac5f825
karl <karl@suma-ev.de>
2016-05-23 09:47:50 +0200

    + Impressum übersetzt

---
+++ resources/lang/en/impressum.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+	'headline.1' => 'SUMA-EV - Verein für freien Wissenszugang (e.V.)',
+	'headline.2' => 'in cooperation with the',
+
+	'info.1' => 'Wikipedia-entry for the',
+	'info.2' => 'Contact',
+	'info.3' => 'encrypted contact form',
+	'info.4' => 'Board',
+	'info.5' => 'Management',
+	'info.6' => 'Youth Protection Commissioner',
+	'info.7' => 'Liable for the content as per § 55 Abs. 2 RStV',
+	'info.8' => 'is a charitable association, registered in the register of associations at the Amtsgericht Hannover
+under',
+	'info.9' => 'sales tax identification number',
+	'info.10' => 'The "Gottfried Wilhelm Leibniz Universität Hannover" is a statutory body.',
+	'info.11' => 'Liability Note',
+	'info.12' => 'Despite thorough content control we do not take responsability for any content of external links. For the content of external links only their operators are reliable.',
+];
