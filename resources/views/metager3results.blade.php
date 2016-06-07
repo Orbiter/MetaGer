a09540d414e98363d2a73db12b24765249f90975
Dominik Pfennig <dominik@suma-ev.de>
2016-06-07 13:41:57 +0200

    View für die Ergebnisseite mit ausschließlich den Ergebnissen, für die Tab generierung

---
+++ resources/views/metager3results.blade.php
@@ -0,0 +1,20 @@
+<div class="col-md-8">
+	{{-- 3-Mal Werbung --}}
+	@for($i = 0; $i <= 2; $i++)
+		@include('layouts.ad', ['ad' => $metager->popAd()])
+	@endfor
+
+	@foreach($metager->getResults()->items() as $result)
+		@if($result->number % 7 === 0)
+		@include('layouts.ad', ['ad' => $metager->popAd()])
+		@endif
+		@include('layouts.result', ['result' => $result])
+	@endforeach
+	<nav class="pager">
+        {!! $metager->getResults()->links() !!}
+    </nav>
+</div>
+<div class="col-md-4" id="quicktips">
+	<iframe class="col-mod-4 hidden-xs hidden-sm" src="/qt?q={{ $metager->getQ() }}"></iframe>
+</div>
+
