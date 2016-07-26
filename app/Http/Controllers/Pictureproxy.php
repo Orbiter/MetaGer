4b7bb498e07761c31bb526ce26abb8f4dc1127be
Dominik Pfennig <dominik@suma-ev.de>
2016-07-26 09:11:57 +0200

    Wenn der PictureProxy ein Bild nicht abrufen kann, wirft er keinen Error mehr

--- app/Http/Controllers/Pictureproxy.php
+++ app/Http/Controllers/Pictureproxy.php
@@ -17,19 +17,24 @@ class Pictureproxy extends Controller
     function get(Request $request) {
         if( $request->has('url') )
         {
-            $file = file_get_contents($request->input('url'));
-            $responseCode = explode(" ", $http_response_header[0])[1];
-            $contentType = "";
-            foreach($http_response_header as $header)
-            {
-                if( strpos($header, "Content-Type:") === 0)
+            try{
+                $file = file_get_contents($request->input('url'));
+                $responseCode = explode(" ", $http_response_header[0])[1];
+                $contentType = "";
+                foreach($http_response_header as $header)
                 {
-                    $tmp = explode(": ", $header);
-                    $contentType = $tmp[1];
+                    if( strpos($header, "Content-Type:") === 0)
+                    {
+                        $tmp = explode(": ", $header);
+                        $contentType = $tmp[1];
+                    }
                 }
+                $response = Response::make($file, $responseCode);
+                $response->header('Content-Type', $contentType);
+            }catch(\ErrorException $e)
+            {
+                $response = Response::make("", 404);
             }
-            $response = Response::make($file, $responseCode);
-            $response->header('Content-Type', $contentType);
             return $response;
         }
     } 
