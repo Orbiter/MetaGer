ad1f800806400bf0209b21fbc237a843fb91f83f
karl <karl@suma-ev.de>
2016-07-27 08:21:47 +0200

    Die Europeana Bildersuche ist jetzt eingebaut. Leider liefert sie meist nur Bilder von Antiquitäten und Kunst, sowie ab und zu kein Vorschaubild, die Links funktionieren dann aber immernoch und zeigen auch ein Bild an.

---
+++ app/Models/parserSkripte/Europeana.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace app\Models\parserSkripte;
+use App\Models\Searchengine;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Europeana extends Searchengine 
+{
+	public $results = [];
+
+	function __construct (\SimpleXMLElement $engine, \App\MetaGer $metager)
+	{
+		parent::__construct($engine, $metager);
+	}
+
+	public function loadResults ($result)
+	{
+		$result = preg_replace("/\r\n/si", "", $result);
+		try {
+			$content = json_decode($result);
+		} catch (\Exception $e) {
+			abort(500, "$result is not a valid json string");
+		}
+		
+		if(!$content)
+		{
+			return;
+		}
+
+		$results = $content->items;
+		foreach($results as $result)
+		{
+			if(isset($result->edmPreview)) {
+				$title = $result->title[0];
+
+				if (preg_match("/(.+)\?.*/si", $result->guid, $match)){
+					$link = $match[1];
+				} else {
+					$link = "";
+				}
+
+				$anzeigeLink = $link;
+				$descr = "";
+
+				/*if (preg_match("/(?:uri=)(.+)/si", urldecode($result->edmPreview[0]), $match)){
+					$image = $match[1];
+				} else {
+					$image = "";
+				}
+				echo $image . "
+";*/
+				$image = urldecode($result->edmPreview[0]);
+				$this->counter++;
+				$this->results[] = new \App\Models\Result(
+					$this->engine,
+					$title,
+					$link,
+					$anzeigeLink,
+					$descr,
+					$this->gefVon,
+					$this->counter,
+					false,
+					$image
+				);
+			}
+		}
+	}
+}
