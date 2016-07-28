82cc6e1ad3decfcec66bbbd28ce557cfc1a2757a
Dominik Pfennig <dominik@suma-ev.de>
2016-07-28 11:36:10 +0200

    Revert "Merge branch '154-confidential-issue' into 'development'"
    This reverts merge request !210

--- resources/views/metager3.blade.php
+++ resources/views/metager3.blade.php
@@ -25,15 +25,11 @@
 		<div class="col-xs-12 col-md-12 resultContainer">
 	@endif
 	@if( !$metager->validated && App::isLocale('de') )
-	<!--/*
-	  *
-	  * Revive Adserver Asynchronous JS Tag
-	  * - Generated with Revive Adserver v3.2.4
-	  *
-	  */-->
-
-	<ins data-revive-zoneid="1" data-revive-id="13c7408a52f1300b059ed2d36a6e0cdd"></ins>
-	<script async src="//ads.metager3.de/www/delivery/asyncjs.php"></script>
+	<div class="mg-panel container" style="margin-bottom:20px;max-height:90px;text-align:left; max-width:100%; padding:0px;margin-top:0px">
+		<a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/spendenaufruf") }}" target="_blank">
+			<img src="/img/aufruf.png" style="max-width:100%;max-height:90px;">
+		</a>
+	</div>
 	@endif
 			{{-- 3-Mal Werbung --}}
 			@for($i = 0; $i <= 2; $i++)
@@ -52,15 +48,6 @@
 	@if( $metager->showQuicktips() )
 		<div class="hidden-xs col-md-4" id="quicktips">
 			<iframe class="col-mod-4 hidden-xs hidden-sm" src="/qt?q={{ $metager->getQ() }}&sprueche={{ $metager->getSprueche() }}"></iframe>
-			<!--/*
-			  *
-			  * Revive Adserver Asynchronous JS Tag
-			  * - Generated with Revive Adserver v3.2.4
-			  *
-			  */-->
-
-			<ins data-revive-zoneid="2" data-revive-id="13c7408a52f1300b059ed2d36a6e0cdd"></ins>
-			<script async src="//ads.metager3.de/www/delivery/asyncjs.php"></script>
 		</div>
 	@endif
 @endsection
