eaf2fa1307649ce6d36d0b9c18ba5f3d30d7a276
Dominik Pfennig <dominik@suma-ev.de>
2016-08-01 14:10:03 +0200

    Partnershop Bezeichnung für Adgoal eingefügt

--- resources/views/layouts/result.blade.php
+++ resources/views/layouts/result.blade.php
@@ -47,10 +47,10 @@
 				<span class="hoster">
 				von {!! $result->gefVon !!}
 				</span>
-				@if( isset($result->partnerShop) )
+				@if( isset($result->partnershop) && $result->partnershop === TRUE )
 				<span class="partnershop-info">
 				<img src="/img/boosticon.png" height="13" alt="Fehler">
-				<a href="/boost">Partnershop</a>
+				<a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/spende") }}">Partnershop</a>
 				</span>
 				@endif
 				<a class="proxy" onmouseover="$(this).popover('show');" onmouseout="$(this).popover('hide');" data-toggle="popover" data-placement="auto right" data-container="body" data-content="Der Link wird anonymisiert geöffnet. Ihre Daten werden nicht zum Zielserver übetragen. Möglicherweise funktionieren manche Webseiten nicht wie gewohnt." href="{{ $result->proxyLink }}" target="{{ $metager->getTab() }}">
