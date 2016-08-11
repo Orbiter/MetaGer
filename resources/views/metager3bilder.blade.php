d4588123055ae3eb7b2355817127e0aefbe42dc0
Dominik Pfennig <dominik@suma-ev.de>
2016-08-11 10:37:24 +0200

    alt Tag für Spendenaufruf eingefügt

--- resources/views/metager3bilder.blade.php
+++ resources/views/metager3bilder.blade.php
@@ -22,7 +22,7 @@
 	@if( !$metager->validated && App::isLocale('de') )
 	<div class="mg-panel container" id="spendenaufruf" style="margin-bottom:20px;max-height:126px;max-width:100%;text-align:center;padding:0px;margin-top:0px">
 					<a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/spendenaufruf") }}">
-					<img src="/img/aufruf.png" style="max-width:100%;max-height:126px;">
+					<img src="/img/aufruf.png" style="max-width:100%;max-height:126px;" alt="Spendenaufruf für die unabhängige, nicht-kommerzielle Internet-Suche" >
 					</a>
 			</div>
 	@endif
