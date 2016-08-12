065a8ac12f70d20ac518218b2dbb45c2472d3fa4
Dominik Pfennig <dominik@suma-ev.de>
2016-08-12 13:06:27 +0200

    Add new file

---
+++ resources/lang/en/partnershops.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+	'heading1'	=>	'MetaGer-Partnershop',
+	'absatz1'	=>	'If the MetaGer result list shows links which have the word "Partnershop" in
+that line, these are at first normal search results.  They are not ranked  
+higher or differently treated than all other search results of MetaGer.  By
+showing these links we do not transfer any of your data to third parties - 
+in the same way we do not transfer such data for ANY search.',
+	'absatz2'	=>	'Behind such links are companies which will pay us with a small amount of
+money if you click on that link.  The result shows the company logo as well.
+By that you additionally recognize our partnershops.  If you click on such  
+link (and ONLY then) the user data will be given to the click address - in  
+the same way as done by clicking the result links of all search engines.  If
+you do not want that, then MetaGer optionally gives the possibility to click
+on "anonym öffnen" (open anonymously).  So you do not have to give your user
+data to third parties.  But in that case we do not receive a share to
+finance MetaGer.  We appeal for a donation in that case, so that we are able
+to run and develope MetaGer as an independent alternative within a
+non-commercial NGO: <a href="https://metager.de/en/spende">https://metager.de/en/spende</a>',
+];
+
