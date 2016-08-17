878d13b8fe8c505b94aa5ce7802f715aad9bb4be
Dominik Pfennig <dominik@suma-ev.de>
2016-08-17 12:49:50 +0200

    Absatz auf englisch eingefügt

--- resources/lang/en/datenschutz.php
+++ resources/lang/en/datenschutz.php
@@ -1,33 +1,40 @@
 <?php
 
 return [
-	'head' => 'Privacy',
+    'head'      => 'Privacy',
 
-	'general.1' => 'For us, privacy is one of the most important goods on the internet. It needs to be preserved and is not to be used for profit. The following is a small summary of our approach. A more detailed description why we are the most secure search engine can be found',
-	'general.2' => 'here',
+    'general.1' => 'For us, privacy is one of the most important goods on the internet. It needs to be preserved and is not to be used for profit. The following is a small summary of our approach. A more detailed description why we are the most secure search engine can be found',
+    'general.2' => 'here (German)',
+    'general.3' => 'Only free software with open source code is under control of any user.  In
+all other cases the users have to BELIEVE what the maintainers tell about
+what is going on inside.  If maintainers claim that they have strictly
+implemented privacy and data protecion, you have to believe them.<br />
+MetaGer is free software under GNU-AGPLv3 Licence. It is available to
+anyone at <a href="https://gitlab.metager3.de/open-source/MetaGer">https://gitlab.metager3.de/open-source/MetaGer</a>. You do not
+have to believe us, YOU might control it yourself!',
 
-	'policy.1' => 'Our Approach/Policy:',
-	'policy.2' => 'Neither do we save your IP, nor the',
-	'policy.3' => '"fingerprint" of your Browser',
-	'policy.4' => '(Which most likely could identify you as well).',
-	'policy.5' => 'We do not set cookies or use tracking-pixel or similar technology, to "track" our users (tracking = following a users digital movement on the internet).',
-	'policy.6' => 'The data transmission from MetaGer is only done encrypted via the https protocol.',
-	'policy.7' => 'We offer access via the anonymous TOR network, the',
-	'policy.8' => 'MetaGer hidden service.',
-	'policy.9' => 'Since the access via the TOR-network seems complicated to many users and sometimes is also slow, we offer a different way, to visit the result pages anonymously: By clicking on the link "open anonymously". Doing this your personal information will be protected on this click and even on all following clicks.',
-	'policy.10' => 'We show as few advertisements as possible, clearly marking them, and financially rely on our users, their',
-	'policy.11' => 'Donations',
-	'policy.12' => 'and membership fees for',
-	'policy.13' => 'Metager is operated and developed by the charitable german organisation',
-	'policy.14' => 'in Ccooperation with the',
-	'policy.15' => 'Leibniz University Hanover',
-	'policy.16' => '.',
-	'policy.17' => 'Our Servers are exclusively located in germany. Therefore they are governed by the german privacy law, which is among the strictest world wide.',
-	'policy.18' => 'Since the',
-	'policy.19' => 'revealments of Edward Snowden in June 2013',
-	'policy.20' => 'numerous search engines claim that they protect theit users privacy, because the user\'s IP adress is not saved. No matter how honourable these descriptions are - fact is that many search engines at least partially host their servers in the USA. Because these search engines are,',
-	'policy.21' => 'as per the Patriot Act and US law',
-	'policy.22' => ', fully unprotected against the access of local authorities. Thus they cannot provide protection of personal data (even if they really try to).',
+    'policy.1'  => 'Our Approach/Policy:',
+    'policy.2'  => 'Neither do we save your IP, nor the',
+    'policy.3'  => '"fingerprint" of your Browser',
+    'policy.4'  => '(Which most likely could identify you as well).',
+    'policy.5'  => 'We do not set cookies or use tracking-pixel or similar technology, to "track" our users (tracking = following a users digital movement on the internet).',
+    'policy.6'  => 'The data transmission from MetaGer is only done encrypted via the https protocol.',
+    'policy.7'  => 'We offer access via the anonymous TOR network, the',
+    'policy.8'  => 'MetaGer hidden service.',
+    'policy.9'  => 'Since the access via the TOR-network seems complicated to many users and sometimes is also slow, we offer a different way, to visit the result pages anonymously: By clicking on the link "open anonymously". Doing this your personal information will be protected on this click and even on all following clicks.',
+    'policy.10' => 'We show as few advertisements as possible, clearly marking them, and financially rely on our users, their',
+    'policy.11' => 'Donations',
+    'policy.12' => 'and membership fees for',
+    'policy.13' => 'Metager is operated and developed by the charitable german organisation',
+    'policy.14' => 'in Ccooperation with the',
+    'policy.15' => 'Leibniz University Hanover',
+    'policy.16' => '.',
+    'policy.17' => 'Our Servers are exclusively located in germany. Therefore they are governed by the german privacy law, which is among the strictest world wide.',
+    'policy.18' => 'Since the',
+    'policy.19' => 'revealments of Edward Snowden in June 2013',
+    'policy.20' => 'numerous search engines claim that they protect theit users privacy, because the user\'s IP adress is not saved. No matter how honourable these descriptions are - fact is that many search engines at least partially host their servers in the USA. Because these search engines are,',
+    'policy.21' => 'as per the Patriot Act and US law',
+    'policy.22' => ', fully unprotected against the access of local authorities. Thus they cannot provide protection of personal data (even if they really try to).',
 
-	'twitter' => 'What others say about our privacy-concept on twitter:',
+    'twitter'   => 'What others say about our privacy-concept on twitter:',
 ];
